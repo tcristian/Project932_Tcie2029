11d290a7c07272afedba7a591127904f560f670e
tcristian <32635580+tcristian@users.noreply.github.com>
2018-04-03 20:56:58 +0300

    Update6

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,53 +1,110 @@
-import controller.LaboratoriesController;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import model.Student;
-import repository.FileDataPersistence;
+package repository;
 
+import controller.LaboratoriesController;
+import junit.framework.TestCase;
+import model.Laboratory;
+import model.Student;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
- * Unit test for simple App.
+ * Created by Alex on 01.04.2018.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-
-        return new TestSuite( AppTest.class );
+public class AppTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
 
     }
-    public void testApp(){
-        assertTrue(true);
+
+    private FileDataPersistence studentsDataPersistance = new FileDataPersistence("students.txt");
+    private FileDataPersistence laboratoryDataPersistance = new FileDataPersistence("laboratories.txt");
+    private LaboratoriesController ctrl = new LaboratoriesController("students.txt", "laboratories.txt");
+
+    public void testSaveStudent() throws Exception {
+        System.out.println("\n TEST - Save student \n");
+
+
+        Student student = new Student("asdf4455", "mircea bravo", 933);
+        ctrl.saveStudent(student);
+        BufferedReader reader = new BufferedReader(new FileReader("students.txt"));
+        String line;
+        line = reader.readLine();
+        String[] temp = line.split(" ");
+        String studentReg = temp[0];
+        String studentName = temp[1] + " " + temp[2];
+        int studentGroup = Integer.valueOf(temp[3]);
+        assertEquals(studentReg, "asdf4455");
+        assertEquals(studentName, "mircea bravo");
+        assertEquals(studentGroup, 933);
+
+
     }
-    public void testAdd(){
-        Student s1 = new Student("123", "Cristian Iftenie", 932);
 
-        FileDataPersistence studentPersistence = new FileDataPersistence(
-                "students.txt");
+    public void testSaveLaboratory() throws Exception {
+        System.out.println("\n TEST - Save Laboratory \n");
 
-        //List<Student> students = studentPersistence.getStudentsList();
-       // Student last = students.get(students.size() - 1)
-        studentPersistence.saveStudent(s1);
-        assertTrue(true);
+        LaboratoriesController ctrl = new LaboratoriesController("students.txt", "laboratories.txt");
+
+        Laboratory laboratory = new Laboratory(10,"11/12/2017", 2, "asdf4455");
+        ctrl.saveLaboratory(laboratory);
+        BufferedReader reader = new BufferedReader(new FileReader("laboratories.txt"));
+        String line = reader.readLine();
+        String[] temp = line.split(" ");
+        int labNumber = Integer.valueOf(temp[0]);
+        int problemNumber = Integer.valueOf(temp[2]);
+        String strudentReg = temp[4];
+        assertEquals(labNumber, 10);
+        assertEquals(problemNumber, 2);
+        assertEquals(strudentReg, "asdf4455");
 
     }
-    /**
-     * Rigourous Test :-)
-     */
+
+    public void testAddGrade() throws Exception {
+        System.out.println("\n TEST - Add new grade \n");
+
+        LaboratoriesController ctrl = new LaboratoriesController("students.txt", "laboratories.txt");
+
+        ctrl.addGrade("asdf4455", "10", 7);
+        BufferedReader reader = new BufferedReader(new FileReader("laboratories.txt"));
+        String line = reader.readLine();
+        String[] temp = line.split(" ");
+        float labGrade = Float.valueOf(temp[3]);
+        assertEquals(labGrade, (float) 7);
+
+    }
+
+    public void testGetLaboratoryMap() throws Exception {
+        System.out.println("TEST - Get laboratory map");
+        //LaboratoriesController ctrl = new LaboratoriesController("students.txt", "laboratories.txt");
+        testAddGrade();
+        Map<String, List<Laboratory>> laboratoryMap = laboratoryDataPersistance.getLaboratoryMap();
+        for(Map.Entry<String, List<Laboratory>> l: laboratoryMap.entrySet()) {
+            assertEquals(l.getKey(), "asdf4455");
+            List<Laboratory> list = l.getValue();
+            assertEquals(list.size(), 1);
+            assertEquals(list.get(0).getNumber(), 10);
+            assertEquals(list.get(0).getGrade(), (float) 7);
+            assertEquals(list.get(0).getProblemNumber(),  2);
+            assertEquals(list.get(0).getStudentRegNumber(), "asdf4455");
+        }
+
+    }
+
+    public void testGetStudentsList() throws Exception {
+        System.out.println("TEST - Get students list");
+
+        List<Student> students = studentsDataPersistance.getStudentsList();
+        assertEquals(students.size(), 1);
+        assertEquals(students.get(0).getName(), "mircea bravo");
+        assertEquals(students.get(0).getGroup(), 933);
+        assertEquals(students.get(0).getRegNumber(), "asdf4455");
+
+    }
+
 }
